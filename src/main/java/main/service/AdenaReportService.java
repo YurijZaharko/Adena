8dc9cd8380a44595deef79a5f36e97baf9717698
YurijZaharko <YurijZaharko@gmail.com>
2018-03-14 16:21:08 +0200

    add transactionl anotation with readOnly flag true

--- src/main/java/main/service/AdenaReportService.java
+++ src/main/java/main/service/AdenaReportService.java
@@ -2,13 +2,19 @@ package main.service;
 
 import main.dto.AdenaReportForm;
 import main.entity.AdenaReport;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
 import java.util.List;
 
+@Transactional
 public interface AdenaReportService {
+
+    @Transactional(readOnly = true)
     List<AdenaReport> findAll();
 
+    @Transactional(readOnly = true)
     List<AdenaReport> findAllFetchAll();
 
     AdenaReportForm addProduct(AdenaReportForm adenaReportForm);
@@ -17,5 +23,6 @@ public interface AdenaReportService {
 
     void delete(Long id);
 
+    @Transactional(readOnly = true, propagation = Propagation.MANDATORY)
     List<AdenaReport> findReportForChart(Calendar threeMonthAgo);
 }
